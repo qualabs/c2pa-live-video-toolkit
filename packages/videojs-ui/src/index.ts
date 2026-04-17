1215cd4a47a90a957c6ac19c305b43add5daf318
Valentina <valentina.giusti@qualabs.com>
2026-04-17 15:56:02 -0300

    feat(videojs-ui): add QualitySelector component

--- packages/videojs-ui/src/index.ts
+++ packages/videojs-ui/src/index.ts
@@ -1,7 +1,9 @@
 export { C2paPlayerUI } from './C2paPlayer.js';
+export { initializeQualitySelector } from './components/QualitySelector.js';
 export type {
   C2paControllerEvents,
   C2paPlayerOptions,
   C2paPlayerInstance,
   VideoJsPlayer,
 } from './types.js';
+export type { QualityInfo, QualitySelectorInstance } from './components/QualitySelector.js';
